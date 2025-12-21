ee6e466b490281e14cee4332f26b9ff4bc827023
Stringybell <christianmartinpileri@gmail.com>
2025-12-21 18:47:03 +0100

    Added Nodetype, Attribute and node Structure

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
-
-#include "stdio.h"
-
-int main(void){
-	printf("Hello world");
-	return 0;
-
-
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
-typedef enum {
-ELEMENT_NODE,
-TEXT_NODE,
-COMMENT_NODE
-}NodeType;
+typedef enum{
+TAG_NODE, // 0: Represents HTML tags such as <div>, <img>, <a>...
+TEXT_NODE, // 1: Represents text
+COMMENT_NODE // 2: Parses text without printing it
+} NodeType;
 
-struct Node{
+/*
+ * Represents the Metadata of the HTML object
+ * and stores it in a linked list
+ */
+typedef struct Attribute{
+	char* type; //e.g. "src","href", "width"
+	char* value;  //e.g. "image.png"
+	struct Attribute* next;
+} Attribute;
 
-}
+/*
+ * Represents the DOM
+ *
+ */
+typedef struct Node{
+	NodeType type;
+	char* tag_name; //html tag, ex. <div>
+	char* text_content;
+	Attribute* attribute;
+	struct Node* child;
+	struct Node* next_sibling;
+	struct Style* style;
+
+} node ;
