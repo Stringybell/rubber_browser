40f1aa21e68cdd72ae84ace93b2689b193a0dd3d
Stringybell <christianmartinpileri@gmail.com>
2025-12-21 18:03:39 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+
+#include "stdio.h"
+
+int main(void){
+	printf("Hello world");
+	return 0;
+
+
+#include "stdlib.h"
+#include "stdio.h"
+
+typedef enum {
+ELEMENT_NODE,
+TEXT_NODE,
+COMMENT_NODE
+}NodeType;
+
+struct Node{
+
+}
